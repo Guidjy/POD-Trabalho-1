77e39fa7f714171a9f235a3272a7d6896fd805ed
Guilherme P. Pimentel <165931160+Guidjy@users.noreply.github.com>
2025-04-19 21:24:34 -0300

    Update lista_encadeada.h

--- lista_encadeada.h
+++ lista_encadeada.h
@@ -3,36 +3,37 @@
 
 #include <stdbool.h>
 
-// estrutura de um nó da lista encadeada
-typedef struct _no {
-    int dado;          // dado contido no nó
-    struct _no *prox;  // ponteiro para o próximo nó da lista
-} *No;
+/* estrutura de um nó da lista encadeada */
+struct _no {
+    int dado;          /* dado contido no nó */
+    struct _no *prox;  /* ponteiro para o próximo nó da lista */
+};
+typedef struct _no *No;
 
-// estrutura do descritor da lista encadeada
-typedef struct _lista_encadeada {
-    int n_elem;  // número de elementos na lista
-    No pri;      // ponteiro para o primeiro elemento da lista
-} *Linked;
+/* estrutura do descritor da lista encadeada */
+struct _lista_encadeada {
+    int n_elem;  /* número de elementos na lista */
+    No pri;      /* ponteiro para o primeiro elemento da lista */
+};
+typedef struct _lista_encadeada *Linked;
 
 
-// cria uma lista encadeada
+/* cria uma lista encadeada */
 Linked linked_cria();
 
-// libera toda a memória alocada para uma lista encadeada
+/* libera toda a memória alocada para uma lista encadeada */
 void linked_destroi(Linked lista);
 
-// retorna true se a lista encadeada estiver vazia
+/* retorna true se a lista encadeada estiver vazia */
 bool linked_vazia(Linked lista);
 
-// insere um elemento no início da lista
+/* insere um elemento no início da lista */
 void linked_insere(Linked lista, int num);
 
-// remove o primeiro elemento da lista e retorna-o
+/* remove o primeiro elemento da lista e retorna-o */
 int linked_remove(Linked lista);
 
-// imprime uma lista encadeada
+/* imprime uma lista encadeada */
 void linked_imprime(Linked lista);
-
 
 #endif
