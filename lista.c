2111a6909987e75c1a776ceb9051fb2c53bd3138
Guilherme P. Pimentel <165931160+Guidjy@users.noreply.github.com>
2025-04-19 21:24:03 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -5,7 +5,7 @@
 
 
 Lista lista_cria(int tamanho) {
-    // aloca memória para a estrutura de uma nova lista
+    /* aloca memória para a estrutura de uma nova lista */
     Lista nova = (Lista) malloc(sizeof(struct _lista));
     if (nova == NULL) {
         printf("Erro na alocação de memória para a struct da lista\n");
@@ -13,7 +13,7 @@ Lista lista_cria(int tamanho) {
     }
     nova->cap = tamanho;
     nova->n_elem = 0;
-    // aloca memória para os elementos da lista
+    /* aloca memória para os elementos da lista */
     nova->vetor = (int*) malloc(sizeof(int) * tamanho);
     if (nova->vetor == NULL) {
         printf("Erro na alocação de memória para os elementos da lista");
@@ -54,8 +54,8 @@ void lista_insere(Lista lista, int num) {
 
 
 int lista_max(Lista lista) {
-    int maior = 0;
-    for (int i = 0; i < lista->n_elem; i++) {
+    int i, maior = 0;
+    for (i = 0; i < lista->n_elem; i++) {
         if (i == 0 || maior < lista->vetor[i]) {
             maior = lista->vetor[i];
         }
@@ -65,7 +65,8 @@ int lista_max(Lista lista) {
 
 
 void lista_preenche(Lista lista, int intervalo) {
-    for (int i = 0; i < lista->cap; i++) {
+    int i;
+    for (i = 0; i < lista->cap; i++) {
         lista->vetor[i] = rand() % intervalo;
     }
     lista->n_elem = lista->cap;
@@ -73,7 +74,7 @@ void lista_preenche(Lista lista, int intervalo) {
 
 
 void lista_swap(Lista lista, int pos_1, int pos_2) {
-    // verifica se os índices de posição são válidos
+    /* verifica se os índices de posição são válidos */
     if ((0 <= pos_1 && pos_1 < lista->n_elem) && (0 <= pos_2 && pos_2 < lista->n_elem)) {
         int temp = lista->vetor[pos_1];
         lista->vetor[pos_1] = lista->vetor[pos_2];
@@ -83,7 +84,8 @@ void lista_swap(Lista lista, int pos_1, int pos_2) {
 
 
 bool lista_ordenada(Lista lista) {
-    for (int i = 1; i < lista->n_elem; i++) {
+    int i;
+    for (i = 1; i < lista->n_elem; i++) {
         if (lista->vetor[i] < lista->vetor[i-1]) {
             printf("Lista não está ordenada ):\n");
             return false;
@@ -95,8 +97,9 @@ bool lista_ordenada(Lista lista) {
 
 
 void lista_imprime(Lista lista) {
+    int i;
     printf("[");
-    for (int i = 0; i < lista->n_elem - 1; i++) {
+    for (i = 0; i < lista->n_elem - 1; i++) {
         printf("%d, ", lista->vetor[i]);
     }
     printf("%d]\n", lista->vetor[lista->n_elem - 1]);
